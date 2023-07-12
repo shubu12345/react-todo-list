a319179393bfab35d6666755439dda33dceecde4
LL\shushind <shubham.shinde@publicisapient.com>
2023-07-12 13:49:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+const itemEvent = (event) => {
+  setInputList(event.target.value);
+}
+
+const listOfItems = () => {
+  setItems((oldItems) => {
+    return [...oldItems, inputList];
+  });
+  setInputList("");
+}
+const deleteItems = (id) => {
+  setItems((oldItems) => {
+    return oldItems.filter((arrEle, index) => {
+      return index !== id;
+    })
+  })
+}
+
+
+return (
+  <>
+  <div className='main-div'>
+  <div className='center-div'>
+  <br />
+  <h1> ToDO List </h1>
+  <br />
+  <input type="text" placeholder='Add a Items' value={inputList} onChange={itemEvent} />
+  <button onClick={listOfItems}> + </button>
+
+  <ol>
+    {items.map((itemval, index) => {
+      // return <li> {itemval} </li>
+      return <ToDoList key={index} id={index} text={itemval} onSelect={deleteItems} />
+    })}
+  </ol>
+  </div>
+  </div>
+  </>
+)
 }
 
 export default App;
